4a3c0acf5bca45d95a7ca945643201ee966d34b1
Pablo <jorellana@dycotein.com>
2026-01-08 17:02:24 -0500

    CHANGE: change format from get-account-movements schema

--- src/core/application/validation/get-account-movements.schema.ts
+++ src/core/application/validation/get-account-movements.schema.ts
@@ -3,10 +3,11 @@ import { z } from "zod";
 export const GetAccountMovementsCommandSchema = z.object({
   accountNumber: z.string().min(1, "El numero de cuenta es obligatorio"),
   productCode: z.string().min(1, "El numero de prducto es obligatorio"),
-  startDate: z.string()
-    .regex(/^\d{2}\/\d{2}\/\d{4}$/, "La fecha de inicio debe tener el formato DD/MM/YYYY o MM/DD/YYYY"),
-  endDate: z.string()
-    .regex(/^\d{2}\/\d{2}\/\d{4}$/, "La fecha de fin debe tener el formato DD/MM/YYYY o MM/DD/YYYY"),
+  //ESTE FORMATO DEBE PERMANECER ASI PARA LA MUTUALISTA
+  FechaInicial: z.string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "La fecha debe tener el formato YYYY-MM-DD"),
+  FechaFinal: z.string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "La fecha debe tener el formato YYYY-MM-DD"),
 });
 
 export type GetAccountMovementsCommandParsed = z.infer<typeof GetAccountMovementsCommandSchema>;
