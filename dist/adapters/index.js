fedad69548a6249aba14315070d19bf2d6b0e009
Fernando Santacruz <lf.santacruz@outlook.es>
2026-03-29 01:06:48 -0500

    chore(release): update dist v3.0.36

--- dist/adapters/index.js
+++ dist/adapters/index.js
@@ -1,1 +1,1 @@
-import"../chunk-GXNAXUDI.js";import{BaseDynamicAdapter as o,executeHttp as r}from"../chunk-HJYKTU4Y.js";import"../chunk-DCG5K4NZ.js";export{o as BaseDynamicAdapter,r as executeHttp};
+import"../chunk-GXNAXUDI.js";import{BaseDynamicAdapter as o,executeHttp as r}from"../chunk-PZXZWDOZ.js";import"../chunk-DCG5K4NZ.js";export{o as BaseDynamicAdapter,r as executeHttp};
